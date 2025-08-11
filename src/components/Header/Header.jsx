d39c131806610216f201ba25f568583f30e5ae07
Vlad Bokii <bokiyvladik10@gmail.com>
2025-08-11 16:09:19 +0300

    fix sprite

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <section className={style.headerBox}>
       <div className={style.svgBox}>
         <svg className={style.svg} width="16" height="16">
-          <use href="/public/sprite/sprite.svg#icon-Logo-Vector"></use>
+          <use href="/sprite/sprite.svg#icon-Logo-Vector"></use>
         </svg>
         <p>MoonPortal</p>
       </div>
