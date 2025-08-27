0b612e03b856089c972e4f76a781f8cce81c0135
Vlad Bokii <bokiyvladik10@gmail.com>
2025-08-27 20:22:30 +0300

    refractor chakra

--- src/components/ChacraSection/ChacraSection.jsx
+++ src/components/ChacraSection/ChacraSection.jsx
@@ -3,6 +3,7 @@ import style from "./ChacraSection.module.css";
 const ChacraSection = () => {
   return (
     <section className={style.section}>
+      <img className={style.cloud} src="/pictures/Cloud.png" />
       <img className={style.stars} src="/pictures/Stars.png" />
       <h3 className={style.title}>The Chakra Portals</h3>
       <p className={style.firstText}>
@@ -15,33 +16,38 @@ const ChacraSection = () => {
         All together they form a system of energy that is the undercurrent of
         the way you move through the world.
       </p>
-      <img className={style.img} src="/pictures/PhotoChakra.png" />
-      <div className={style.formBox}>
-        <p className={style.chakraText}>Get the Free MoonPortal Guidebooks</p>
-        <p className={style.underChakraText}>
-          Connect to the cycles of the moon for your spiritual journey{" "}
-        </p>
-        <div className={style.labelBox}>
-          <label className={style.label}>
-            Name
-            <input
-              className={style.input}
-              type="text"
-              placeholder="Your Name"
-            />
-          </label>
 
-          <label className={style.label}>
-            Email
-            <input
-              className={style.input}
-              type="email"
-              placeholder="Your Email"
-            />
-          </label>
+      <div className={style.formBox}>
+        <img className={style.chakra} src="/pictures/PhotoChakra.png" />
+        <div>
+          <p className={style.chakraText}>Get the Free MoonPortal Guidebooks</p>
+          <p className={style.underChakraText}>
+            Connect to the cycles of the moon for your spiritual journey{" "}
+          </p>
+          <div className={style.labelBox}>
+            <label className={style.label}>
+              Name
+              <input
+                className={style.input}
+                type="text"
+                placeholder="Your Name"
+              />
+            </label>
+
+            <label className={style.label}>
+              Email
+              <input
+                className={style.input}
+                type="email"
+                placeholder="Your Email"
+              />
+            </label>
+            <button className={style.but} type="submit">
+              Subscribe Now
+            </button>
+          </div>
         </div>
       </div>
-      <button type="submit">Get Guidebooks</button>
     </section>
   );
 };
